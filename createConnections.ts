ae1ad88f1f0a6bdda27179f23c433646e710bde2
Prateek Saikia <34713296+Prateekj7@users.noreply.github.com>
2021-10-23 12:12:35 +0530

    deleted redundant

--- createConnections.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectionOptions, createConnection } from "typeorm";
-import { Hotels_Csv } from "./hotels_csv";
-
-createConnection(<ConnectionOptions>{
-    url: process.env.DATABASE_URL,
-    entities: [Hotels_Csv],
-    type: "postgres"
-});
